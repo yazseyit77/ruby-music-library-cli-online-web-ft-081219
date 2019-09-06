267ec5b8e1eb34daa88c94ecbce35970a8699754
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-06 14:18:38 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -7,38 +7,39 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
+      input = ''
 
-    answer = ""
+      while input != 'exit'
+        puts "Welcome to your music library!"
+        puts "To list all of your songs, enter 'list songs'."
+        puts "To list all of the artists in your library, enter 'list artists'."
+        puts "To list all of the genres in your library, enter 'list genres'."
+        puts "To list all of the songs by a particular artist, enter 'list artist'."
+        puts "To list all of the songs of a particular genre, enter 'list genre'."
+        puts "To play a song, enter 'play song'."
+        puts "To quit, type 'exit'."
+        puts "What would you like to do?"
+        input = gets.chomp
 
-    while answer != 'exit'
-
-      puts "What would you like to do?"
-
-    answer = gets.strip
-    case answer
-    when 'list songs'
-      songs
-    when 'list artists'
-      artists
-    when 'list genres'
-      genres
-    when 'list artist'
-      list_artist
-    when 'list genre'
-      list_genre
-    when 'play song'
-      play_song
-    end
-    end
-  end
+        case input
+        when 'list songs'
+          self.list_songs
+        when 'list artists'
+          self.list_artists
+        when 'list genres'
+          self.list_genres
+        when 'list artists'
+          self.list_artists
+        when 'list artist'
+          self.list_songs_by_artist
+        when 'list genre'
+          self.list_songs_by_genre
+        when 'play song'
+          self.play_song
+        else
+          "Type in a valid request please"
+        end
+      end
 
   def list_songs
     Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
