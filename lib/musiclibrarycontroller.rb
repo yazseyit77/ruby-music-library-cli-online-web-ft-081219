76c1ed3734a5f1a961e696681db77bd084b94188
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-06 13:26:52 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -42,7 +42,7 @@ class MusicLibraryController
 
   def list_songs
     Song.all.each_with_index do |song, index|
-    puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
 
