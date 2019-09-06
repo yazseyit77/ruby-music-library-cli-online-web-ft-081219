a675aa4529cdc28afc174a003005887d2a1ef24c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-06 13:59:31 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -59,7 +59,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
       input = gets.strip
-  
+
       if artist = Artist.find_by_name(input)
         artist.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
           puts "#{i}. #{song.name} - #{song.genre.name}"
