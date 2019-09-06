6750ca750a0d9af255fb7bb42cd96497d270c19f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-06 13:18:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.detect { |song| song.name == name}
+    @@all.detect{|s| s.name == name}
   end
 
   def self.find_or_create_by_name(name)
